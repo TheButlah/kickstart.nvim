0a101dd8cc64ceaca8469928d9759ee174f8aebb
Ryan Butler <thebutlah@gmail.com>
2023-06-18 17:13:43 -0400

    Telescope search for hidden files

--- init.lua
+++ init.lua
@@ -280,6 +280,11 @@ require('telescope').setup {
       },
     },
   },
+  pickers = {
+    find_files = {
+      find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+    },
+  },
 }
 
 -- Enable telescope fzf native, if installed
